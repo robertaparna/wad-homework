e302d83a057da277e792833c50af21edf2856d26
Andra <andra.bluum@ut.ee>
2022-11-02 11:10:18 +0200

    Fixed dropdown

--- JavaScript.js
+++ JavaScript.js
@@ -1,17 +1,4 @@
-function myFunction() {
-    document.getElementById("dropdown").classList.toggle(".dropdown.open-menu");
+function dropdownMenu() {
+  const menu = document.querySelector('.dropdown-menu');
+  menu.classList.toggle('active');
   }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  /*window.onclick = function(event) {
-    if (!event.target.matches(' .material-symbols-outlinedpbtn')) {
-      var dropdowns = document.getElementsByClassName(" .dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }*/
