c5b730f8d20b971b63c6376c5355a1c172433b27
robertaparna <roberta.parna@wisercat.net>
2022-11-23 22:18:40 +0200

    merge fixes

--- wad-homework/src/components/AddPost.vue
+++ wad-homework/src/components/AddPost.vue
@@ -1,8 +1,16 @@
 <template>
-
+  <div class="container">
+      <h1>Add Post</h1>
+      <div class="body">
+        <label>Body</label>
+        <input type="text" placeholder="body">
+      </div>
+    <button>Add</button>
+</div>
 </template>
 
 <script>
+import '../assets/addPost.css'
 export default {
   name: "AddPost"
 }
