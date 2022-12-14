26bac77db2e4a8b0297ce121ba5b51f7b9cf26b0
Andra <andra.bluum@ut.ee>
2022-12-14 13:16:43 +0200

    "Starting single posts page"

---
+++ wad-homework/src/components/PostPage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="SinglePost">
+        <div id="form">
+            <input name="date" type="image" id="image" required v-model="post.picture" />
+            <input name="file" type="image" id="image" required v-model="post.picture" />
+            <input name="body" type="text" id="body" required v-model="post.body" />
+            <input name="likes" type="button" id="likes" required v-model="post.likes" />
+        </div>
+    </div>
+</template>
